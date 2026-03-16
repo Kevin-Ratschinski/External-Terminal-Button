0b5fbfbbda6288970113aefb4dd8dd235e6b9123
Kevin Ratschinski <k.ratschinski@icloud.com>
2026-03-16 18:45:40 +0100

    feat: enhance external terminal button functionality and configuration

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,45 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+const OPEN_EXTERNAL_TERMINAL_COMMAND = "externalTerminalButton.openExternalTerminal";
+const OPEN_NATIVE_CONSOLE_COMMAND = "workbench.action.terminal.openNativeConsole";
+const STATUS_BAR_NAME = "Open External Terminal";
+const CONFIG_SECTION = "externalTerminalButton";
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "external-terminal-button" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('external-terminal-button.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from External Terminal Button!');
-	});
-
-	context.subscriptions.push(disposable);
+export async function openExternalTerminal(): Promise<void> {
+  await vscode.commands.executeCommand(OPEN_NATIVE_CONSOLE_COMMAND);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+function getConfig(): vscode.WorkspaceConfiguration {
+  return vscode.workspace.getConfiguration(CONFIG_SECTION);
+}
+
+function createStatusBarItem(): vscode.StatusBarItem {
+  const config = getConfig();
+  const alignment = config.get<string>("alignment") === "right" ? vscode.StatusBarAlignment.Right : vscode.StatusBarAlignment.Left;
+  const priority = config.get<number>("priority", 0);
+  const text = config.get<string>("text", "$(terminal) Ext Terminal");
+
+  const terminalItem = vscode.window.createStatusBarItem(alignment, priority);
+  terminalItem.name = STATUS_BAR_NAME;
+  terminalItem.text = text;
+  terminalItem.tooltip = "Open New External Terminal";
+  terminalItem.command = OPEN_EXTERNAL_TERMINAL_COMMAND;
+
+  return terminalItem;
+}
+
+export function activate(context: vscode.ExtensionContext): void {
+  const openTerminalCommand = vscode.commands.registerCommand(OPEN_EXTERNAL_TERMINAL_COMMAND, openExternalTerminal);
+  let terminalItem = createStatusBarItem();
+  terminalItem.show();
+
+  const configListener = vscode.workspace.onDidChangeConfiguration((e) => {
+    if (e.affectsConfiguration(CONFIG_SECTION)) {
+      terminalItem.dispose();
+      terminalItem = createStatusBarItem();
+      terminalItem.show();
+    }
+  });
+
+  context.subscriptions.push(openTerminalCommand, terminalItem, configListener);
+}
